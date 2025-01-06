9d668f048c0ad47916c1a9ad339410fd3331810d
YehorLitvinov <71943574+Weux94@users.noreply.github.com>
2025-01-06 12:26:13 +0200

    Delete hm_7.1.py

--- hm_7.1.py
+++
@@ -1,6 +0,0 @@
-name=input("Enter your name: ")
-age=int(input("Enter your age: "))
-def say_hi (name, age):
-    return f"Hello, my name is {name}! I am {age} years old."
-
-print(say_hi(name, age))
