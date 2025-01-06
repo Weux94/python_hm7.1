486806fdaef4c01be8a827f1955300e657155d6d
YehorLitvinov <71943574+Weux94@users.noreply.github.com>
2025-01-06 12:27:40 +0200

    Add files via upload

---
+++ hm_7.1.py
@@ -0,0 +1,8 @@
+
+def say_hi (name, age):
+    return f"Hello, my name is {name}! I am {age} years old."
+
+user_name = input("Enter your name: ")
+user_age = input("Enter your age: ")
+
+print(say_hi(user_name, user_age))
